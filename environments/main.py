d87d02e0114604f098cf776b892ab887fe2d5bde
EirikReiestad <eirik@reiestad.no>
2024-06-19 23:59:58 +0200

    gym

--- environments/main.py
+++
@@ -1,5 +0,0 @@
-# from examples.snake import main as snake_main
-from examples.maze import main as maze_main
-
-if __name__ == "__main__":
-    maze_main()
--- gymnasium/readme.md
+++
@@ -1,2 +0,0 @@
-The code are highly inspired and mostly copied from the following sources:
-[Gymnasium](https://github.com/Farama-Foundation/Gymnasium/)
