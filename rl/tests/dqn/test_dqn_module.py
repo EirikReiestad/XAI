4725b2687d05205a13a16cc8d7860d4a8ebdbacf
EirikReiestad <eirik@reiestad.no>
2024-06-20 16:11:22 +0200

    test

--- rl/tests/dqn/test_dqn_module.py
+++ rl/tests/dqn/test_dqn_module.py
@@ -39,7 +39,7 @@ class TestDQNModule(unittest.TestCase):
     def test_train(self):
         state = torch.zeros(1, self.n_observations)
         action = torch.tensor([[0]], dtype=torch.long)
-        observation = np.array([0.0, 0.0, 0.0, 0.0])
+        observation = np.array(1, [0.0, 0.0, 0.0, 0.0])
         reward = 1.0
         terminated = False
         truncated = False
@@ -52,7 +52,7 @@ class TestDQNModule(unittest.TestCase):
     def test_train_terminated(self):
         state = torch.zeros(1, self.n_observations)
         action = torch.tensor([[0]], dtype=torch.long)
-        observation = np.array([0.0, 0.0, 0.0, 0.0])
+        observation = np.array(1, [0.0, 0.0, 0.0, 0.0])
         reward = 1.0
         terminated = True
         truncated = False
