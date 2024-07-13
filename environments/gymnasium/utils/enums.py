c78852b3d9c259bc1dd256b4e5ef19e3c3413022
EirikReiestad <eirik@reiestad.no>
2024-07-13 10:39:41 +0200

    feat: Cleaned up file structure

--- environments/gymnasium/utils/enums.py
+++ environments/gymnasium/utils/enums.py
@@ -2,57 +2,6 @@ import enum
 
 
 @enum.unique
-class MazeTileType(enum.Enum):
-    EMPTY = 0
-    OBSTACLE = 1
-    START = 2
-    END = 3
-
-    def __str__(self):
-        if self == MazeTileType.EMPTY:
-            return "Empty"
-        if self == MazeTileType.OBSTACLE:
-            return "Obstacle"
-        if self == MazeTileType.START:
-            return "Start"
-        if self == MazeTileType.END:
-            return "End"
-
-    def __add__(self, other):
-        new_value = (self.value + other) % len(MazeTileType)
-        return MazeTileType(new_value)
-
-    def __iadd__(self, other):
-        new_value = (self.value + other) % len(MazeTileType)
-        return MazeTileType(new_value)
-
-
-@enum.unique
-class MazeDrawMode(enum.Enum):
-    NOTHING = 0
-    OBSTACLE = 1
-    ERASE = 2
-    START = 3
-    END = 4
-
-    def __str__(self):
-        if self == MazeDrawMode.NOTHING:
-            return "Nothing"
-        if self == MazeDrawMode.OBSTACLE:
-            return "Obstacle"
-        if self == MazeDrawMode.ERASE:
-            return "Erase"
-
-    def __add__(self, other):
-        new_value = (self.value + other) % (len(MazeDrawMode)-2)
-        return MazeDrawMode(new_value)
-
-    def __iadd__(self, other):
-        new_value = (self.value + other) % (len(MazeDrawMode)-2)
-        return MazeDrawMode(new_value)
-
-
-@enum.unique
 class Direction(enum.Enum):
     UP = 0
     RIGHT = 1
@@ -70,6 +19,7 @@ class Direction(enum.Enum):
             return (-1, 0)
 
 
+@enum.unique
 class Color(enum.Enum):
     RED = (255, 0, 0)
     GREEN = (0, 255, 0)
--- environments/gymnasium/utils/position.py
+++ environments/gymnasium/utils/position.py
@@ -1,9 +1,4 @@
-"""
-Utility functions used for Atari2D environments.
-"""
-
 from dataclasses import dataclass
-import random
 
 
 @dataclass
@@ -53,30 +48,3 @@ class Position:
 
     def to_tuple(self):
         return (self.x, self.y)
-
-
-DIRECTIONS = {
-    0: (0, 1),  # Up
-    1: (1, 0),  # Right
-    2: (0, -1),  # Down
-    3: (-1, 0),  # Left
-}
-
-
-def generate_random_position(width: int, height: int, other: [Position] = None) -> Position:
-    if not isinstance(width, int) or not isinstance(height, int):
-        raise ValueError("Width and height should be integers.")
-    count = 0
-    position = Position(random.randint(0, width-1),
-                        random.randint(0, height-1))
-    if other is None:
-        return position
-
-    while position in other:
-        count += 1
-        if count > width * height:
-            raise ValueError(
-                "Could not generate a random position.")
-        position = Position(random.randint(0, width-1),
-                            random.randint(0, height-1))
-    return position
