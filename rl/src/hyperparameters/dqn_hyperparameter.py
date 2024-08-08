7fa3d3a5b92fff41ba9af67860b133bf5dd13342
Eirik Reiestad <eirik@reiestad.no>
2024-08-08 18:26:01 +0200

    hyp

--- rl/src/hyperparameters/dqn_hyperparameter.py
+++ rl/src/hyperparameters/dqn_hyperparameter.py
@@ -5,7 +5,7 @@ from dataclasses import dataclass
 
 @dataclass
 class DQNHyperparameter:
-    lr = 0.1
+    lr = 0.01
     gamma = 0.999
     eps_start = 0.9
     eps_end = 0.05
@@ -14,7 +14,7 @@ class DQNHyperparameter:
     tau = 0.001
 
     def save(self, path):
-        with open(path, 'w') as f:
+        with open(path, "w") as f:
             f.write(str(self))
 
     def load(self, path):
@@ -24,10 +24,9 @@ class DQNHyperparameter:
             logging.warning(f"Hyperparameters not found at {path}")
             self.save(path)
 
-        with open(path, 'r') as f:
+        with open(path, "r") as f:
             data = f.read()
-            lr, gamma, eps_start, eps_end, eps_decay, batch_size, tau = data.split(
-                ',')
+            lr, gamma, eps_start, eps_end, eps_decay, batch_size, tau = data.split(",")
 
     def __str__(self):
         return f"{self.lr},{self.gamma},{self.eps_start},{self.eps_end},{self.eps_decay},{self.batch_size},{self.tau}"
