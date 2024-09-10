4ea7dd4854b7a9817a2cf93ee8799fe2591815fc
Eirik Reiestad <eirik@reiestad.no>
2024-09-10 12:01:13 +0200

    feat: wandb

--- demo/src/common/episode_information.py
+++ demo/src/common/episode_information.py
@@ -6,3 +6,10 @@ class EpisodeInformation:
     durations: list[int]
     rewards: list[float]
     object_moved_distance: list[float]
+
+    def last_episode(self, prefix: str = ""):
+        return {
+            f"{prefix}durations": self.durations[-1],
+            f"{prefix}rewards": self.rewards[-1],
+            f"{prefix}object_moved_distance": self.object_moved_distance[-1],
+        }
