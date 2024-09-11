cbe66adfb8824744cc25d3f00380ee9472beacc1
Eirik Reiestad <eirik@reiestad.no>
2024-09-11 17:32:02 +0200

    refactoring

--- rl/src/common/check.py
+++ rl/src/common/check.py
@@ -4,23 +4,27 @@ import torch
 
 def raise_if_not_same_shape(
     a: np.ndarray | torch.Tensor,
-    b: np.ndarray | torch.Tensor,
+    b: np.ndarray | torch.Tensor | tuple[int, ...] | None,
     name_a: str = "a",
     name_b: str = "b",
 ):
-    if a.shape != b.shape:
-        raise ValueError(
-            f"Shape of {name_a}: {a.shape} is not same as {name_b}: {b.shape}"
-        )
+    if b is None:
+        raise ValueError(f"{name_b} is None, but expected to be not None.")
+    if isinstance(b, tuple):
+        if a.shape != b:
+            raise ValueError(f"Shape of {name_a}: {a.shape} is not {b}.")
+    else:
+        if a.shape != b.shape:
+            raise ValueError(
+                f"Shape of {name_a}: {a.shape} is not same as {name_b}: {b.shape}"
+            )
 
 
 def raise_if_not_all_same_shape(
     a_vec: list[np.ndarray] | list[torch.Tensor],
-    b: np.ndarray | torch.Tensor,
+    b: np.ndarray | torch.Tensor | tuple[int, ...] | None,
     name_a: str = "a",
     name_b: str = "b",
 ):
-    if not all(a.shape == b.shape for a in a_vec):
-        raise ValueError(
-            f"All {name_a} must have shape {b.shape}, but found a mismatch."
-        )
+    for a in a_vec:
+        raise_if_not_same_shape(a, b, name_a, name_b)
