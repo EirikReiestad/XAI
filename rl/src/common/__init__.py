4c51fb65e5b59118b3750452d652e36f4e0d14fe
Eirik Reiestad <eirik@reiestad.no>
2024-09-11 15:45:30 +0200

    feat: making the code more modular and seperate it from the class where it can be in an own function

--- rl/src/common/__init__.py
+++ rl/src/common/__init__.py
@@ -1,1 +1,3 @@
 from .conv_layer import ConvLayer
+
+__all__ = ["ConvLayer"]
--- rl/src/dqn/managers/memory_manager.py
+++ rl/src/dqn/managers/memory_manager.py
@@ -1,7 +1,9 @@
+from rl.src.dqn.components.memory import PrioritizedReplayMemory
+
+
 class MemoryManager:
     def __init__(self, memory_size: int):
         self.memory_size = memory_size
-        self.memory = deque(maxlen=memory_size)
 
     def initialize(self):
-        pass
+        self.memory = PrioritizedReplayMemory(self.memory_size)
