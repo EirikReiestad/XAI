12181324afc8aad84b921dd3b1029d8dabbcff9e
Eirik Reiestad <eirik@reiestad.no>
2024-09-12 14:56:36 +0200

    cleanup: removed snake env

--- demo/settings.py
+++ demo/settings.py
@@ -1,7 +1,7 @@
 from demo.src.demos.demo_type import DemoType
 from demo.src.demos.rl_type import RLType
 
-DEMO_TYPE = DemoType.CARTPOLE
+DEMO_TYPE = DemoType.SNAKE
 RL_TYPE = RLType.DQN
 
 EPOCHS = 500
