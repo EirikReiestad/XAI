210b24844274ca048f1f589f94dd1bc3e2d69390
Eirik Reiestad <eirik@reiestad.no>
2024-09-13 16:14:29 +0200

    qmap

--- demo/src/demos/multi_agent/tag.py
+++ demo/src/demos/multi_agent/tag.py
@@ -12,6 +12,8 @@ from demo.src.plotters import Plotter
 from environments.gymnasium.wrappers import MultiAgentEnv
 from renderer import Renderer
 from rl.src.dqn.wrapper import MultiAgentDQN
+from rl.src.common.getter import get_torch_from_numpy
+from rl.src.dqn.common.q_values_map import get_q_values_map
 
 # Set up matplotlib
 is_ipython = "inline" in matplotlib.get_backend()
@@ -31,66 +33,71 @@ class TagDemo:
         self.env: MultiAgentEnv = MultiAgentEnv(env)
 
     def run(self):
-        dqn = MultiAgentDQN(self.env, 2, "dqnpolicy", wandb=True)
+        dqn = MultiAgentDQN(self.env, 2, "dqnpolicy", wandb=False)
 
         plt.ion()
 
         self.env.reset()
 
-        try:
-            for i_episode in range(1000):
-                dqn.learn(1)
+        for i_episode in range(1000):
+            dqn.learn(1)
 
-                state, _ = self.env.reset()
+            state, _ = self.env.reset()
+            state = torch.tensor(state, device=device, dtype=torch.float32).unsqueeze(0)
+
+            agent_rewards = [0, 0]
+
+            for t in count():
+                predicted_actions = dqn.predict(state)
+                actions = [action.item() for action in predicted_actions]
+                (
+                    full_state,
+                    observation,
+                    terminated,
+                    observations,
+                    rewards,
+                    terminals,
+                    truncated,
+                    _,
+                ) = self.env.step_multiple(actions)
+
+                agent_rewards += rewards
+
                 state = torch.tensor(
-                    state, device=device, dtype=torch.float32
+                    observation, device=device, dtype=torch.float32
                 ).unsqueeze(0)
 
-                agent_rewards = [0, 0]
+                rgb = self.env.render()
 
-                for t in count():
-                    predicted_actions = dqn.predict(state)
-                    actions = [action.item() for action in predicted_actions]
-                    (
-                        full_state,
-                        observation,
-                        terminated,
-                        observations,
-                        rewards,
-                        terminals,
-                        truncated,
-                        _,
-                    ) = self.env.step_multiple(actions)
+                all_possible_states = self.env.get_wrapper_attr(
+                    "get_all_possible_states"
+                )()
 
-                    agent_rewards += rewards
+                states = np.zeros(
+                    (len(all_possible_states[0]), len(all_possible_states)),
+                    dtype=torch.Tensor,
+                )
+                for i, row in enumerate(all_possible_states):
+                    for j, column in enumerate(row):
+                        column = np.array(column, dtype=np.float32)
+                        torch_column = get_torch_from_numpy(column)
+                        states[j, i] = torch_column
 
-                    state = torch.tensor(
-                        full_state, device=device, dtype=torch.float32
-                    ).unsqueeze(0)
+                q_values = dqn.get_q_values(states, 0)
+                q_values_map = get_q_values_map(states=full_state, q_values=q_values)
 
-                    rgb = self.env.render()
+                if isinstance(rgb, np.ndarray):
+                    self.renderer.render(background=rgb, q_values=q_values_map)
 
-                    all_possible_states = self.env.get_wrapper_attr(
-                        "get_all_possible_states"
-                    )
-
-                    q_values = dqn.get_q_values(all_possible_states, 0)
-                    print(rgb)
-                    if isinstance(rgb, np.ndarray):
-                        self.renderer.render(background=rgb, q_values=q_values)
-
-                    if terminated:
-                        self.episode_information.durations.append(t + 1)
-                        self.episode_information.rewards.append(agent_rewards[0])
-                        self.plotter.update(self.episode_information)
-                        break
-        except Exception as e:
-            logging.error(e)
-        finally:
-            self.env.close()
-            logging.info("Complete")
-            self.plotter.update(self.episode_information, show_result=True)
-            plt.ioff()
+                if terminated or any(terminals) or any(truncated):
+                    self.episode_information.durations.append(t + 1)
+                    self.episode_information.rewards.append(agent_rewards[0])
+                    self.plotter.update(self.episode_information)
+                    break
+        self.env.close()
+        logging.info("Complete")
+        self.plotter.update(self.episode_information, show_result=True)
+        plt.ioff()
         plt.show()
 
 
