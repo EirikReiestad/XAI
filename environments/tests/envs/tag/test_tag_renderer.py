c696c632985e04822b56fb3251c3b4469703d045
Eirik Reiestad <eirik@reiestad.no>
2024-09-17 12:13:04 +0200

    tag tests

---
+++ environments/tests/envs/tag/test_tag_renderer.py
@@ -0,0 +1,46 @@
+import unittest
+import numpy as np
+import pygame as pg
+from environments.gymnasium.envs.tag.tag_renderer import TagRenderer
+from environments.gymnasium.envs.tag.utils import TileType
+
+
+class TestTagRenderer(unittest.TestCase):
+    def setUp(self):
+        self.renderer = TagRenderer(10, 10, 300, 300)
+        self.state = np.zeros((10, 10), dtype=np.uint8)
+        self.state[0, 0] = TileType.OBSTACLE.value
+        self.state[1, 1] = TileType.BOX.value
+        self.state[2, 2] = TileType.SEEKER.value
+        self.state[3, 3] = TileType.HIDER.value
+
+    def test_init_render(self):
+        self.assertTrue(self.renderer.is_open)
+
+    def test_init_render_mode(self):
+        self.renderer.init_render_mode("rgb_array")
+        self.assertEqual(self.renderer.render_mode, "rgb_array")
+        with self.assertRaises(ValueError):
+            self.renderer.init_render_mode("invalid_mode")
+
+    def test_render_rgb_array(self):
+        self.renderer.init_render_mode("rgb_array")
+        result = self.renderer.render(self.state)
+        assert result is not None
+        self.assertIsInstance(result, np.ndarray)
+        self.assertEqual(result.shape, (300, 300, 3))
+
+    def test_render_human(self):
+        self.renderer.init_render_mode("human")
+        result = self.renderer.render(self.state)
+        self.assertIsNone(result)
+
+    def test_close(self):
+        self.renderer.close()
+        self.assertFalse(self.renderer.is_open)
+        # Check if pygame quit properly
+        self.assertFalse(pg.get_init())
+
+
+if __name__ == "__main__":
+    unittest.main()
---
+++ environments/tests/envs/tag/utils/test_full_state_data_extractor.py
@@ -0,0 +1,52 @@
+import unittest
+import numpy as np
+from environments.gymnasium.envs.tag.utils import AgentType
+from environments.gymnasium.envs.tag.utils.tile_type import TileType
+from environments.gymnasium.envs.tag.utils.agent_tile_type import AGENT_TILE_TYPE
+from environments.gymnasium.utils import Position
+from environments.gymnasium.envs.tag.utils import FullStateDataExtractor
+
+
+class TestFullStateDataExtractor(unittest.TestCase):
+    def setUp(self):
+        self.state = np.array([[0, 1, 2], [3, 4, 5], [6, 7, 8]])
+        self.agent_type = AgentType.SEEKER
+        AGENT_TILE_TYPE[self.agent_type] = 1
+        self.tile_type = TileType.EMPTY  # .value = 0
+
+    def test_get_agent_position(self):
+        AGENT_TILE_TYPE[self.agent_type] = 1
+        position = FullStateDataExtractor.get_agent_position(
+            self.state, self.agent_type
+        )
+        self.assertEqual(position, Position(x=1, y=0))
+
+    def test_get_agent_position_raises_error_if_multiple_agents(self):
+        self.state[1, 1] = AGENT_TILE_TYPE[self.agent_type]
+        with self.assertRaises(ValueError):
+            FullStateDataExtractor.get_agent_position(self.state, self.agent_type)
+
+    def test_agent_exist(self):
+        AGENT_TILE_TYPE[self.agent_type] = 1
+        exists = FullStateDataExtractor.agent_exist(self.state, self.agent_type)
+        self.assertTrue(exists)
+
+    def test_agent_exist_returns_false_if_agent_does_not_exist(self):
+        AGENT_TILE_TYPE[self.agent_type] = 9
+        exists = FullStateDataExtractor.agent_exist(self.state, self.agent_type)
+        self.assertFalse(exists)
+
+    def test_get_positions(self):
+        self.state[1, 2] = self.tile_type.value
+        positions = FullStateDataExtractor.get_positions(self.state, self.tile_type)
+        self.assertEqual(positions, [Position(x=2, y=1)])
+
+    def test_is_empty_tile(self):
+        empty_position = Position(x=0, y=0)
+        self.assertTrue(
+            FullStateDataExtractor.is_empty_tile(self.state, empty_position)
+        )
+
+
+if __name__ == "__main__":
+    unittest.main()
