f01564f3d59305a3b543a5be1cb2af652b046312
Eirik Reiestad <eirre@ntnu.no>
2024-09-18 14:24:36 +0200

    idunsh

---
+++ idun.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+#SBATCH --partition=CPUQ
+#SBATCH --account=eirre
+#SBATCH --time=0-00:15:00     # 0 days and 15 minutes limit
+#SBATCH --nodes=1             # 1 compute nodes
+#SBATCH --cpus-per-task=1     # 2 CPU cores
+#SBATCH --mem=5G              # 5 gigabytes memory
+#SBATCH --output=log.txt    # Log file
+
+module load Python/3.11.5-GCCcore-13.2.0
+poetry run python demo
