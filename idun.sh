065e509dac4aacc8a15d5e46ad5f2d41856dc46f
Eirik Reiestad <eirik@reiestad.no>
2024-09-18 17:58:53 +0200

    idun:config

--- idun.sh
+++ idun.sh
@@ -3,9 +3,8 @@
 #SBATCH --account=eirre
 #SBATCH --time=0-00:15:00     # 0 days and 15 minutes limit
 #SBATCH --nodes=1             # 1 compute nodes
-#SBATCH --cpus-per-task=1     # 2 CPU cores
-#SBATCH --mem=5G              # 5 gigabytes memory
-#SBATCH --output=log.txt    # Log file
+#SBATCH --gres=gpu:1          # 1 GPU
+#SBATCH --output=log.txt      # Log file
 
 module purge
 module load Python/3.11.5-GCCcore-13.2.0
