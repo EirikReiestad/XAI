bc6aa7f0513bea553f3303b7cd22e1fdd4ed32ab
Eirik Reiestad <eirik@reiestad.no>
2024-09-24 15:28:33 +0200

    idun: params

--- idun.sh
+++ idun.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
-#SBATCH --partition=CPUQ
+#SBATCH --partition=GPUQ
 #SBATCH --account=eirre
-#SBATCH --time=0-10:00:00     # 0 days and 15 minutes limit
-#SBATCH --nodes=1             # 1 compute nodes
-#SBATCH --gres=gpu:1          # 1 GPU
-#SBATCH --output=log.txt      # Log file
+#SBATCH --time=0-24:00:00
+#SBATCH --nodes=2
+#SBATCH --gres=gpu:1
+#SBATCH --output=log.txt
 
 module purge
 module load Python/3.11.5-GCCcore-13.2.0
