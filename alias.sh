a645280bc70ae9712671506366cfee1b87ceeff0
Eirik Reiestad <eirik@reiestad.no>
2024-09-25 14:01:37 +0200

    idun:alias

---
+++ alias.sh
@@ -0,0 +1,7 @@
+# This should be put in .bashrc on the cluster
+alias get_quota='lfs quota -u $(whoami) /cluster'
+alias getacc='sacctmgr show assoc where user=$(whoami) format=account --parsable2 --noheader'
+alias getdefaultacc='sacctmgr show user $(whoami) format=defaultaccount --parsable2 --noheader'
+alias gowork='cd /cluster/work/$(whoami)/'
+alias past_jobs='sacct -X --format=JobID,Jobname%30,state,time,elapsed,nnodes,ncpus,nodelist,AllocTRES%50'
+alias queue='watch -n 5 "squeue --me --format=\"%.18i %.9P %.30j %.8u %.8T %.10M %.9l %.6D %.19S %R\""'
