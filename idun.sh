47b4ec36ce383c819827cb54a490d89bdd25e1d7
Eirik Reiestad <eirik@reiestad.no>
2024-09-25 13:57:32 +0200

    idun:

--- idun.sh
+++ idun.sh
@@ -1,10 +1,20 @@
 #!/bin/sh
+#SBATCH --account="ie-idi"
 #SBATCH --partition=GPUQ
-#SBATCH --account=eirre
 #SBATCH --time=0-24:00:00
-#SBATCH --nodes=2
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
 #SBATCH --gres=gpu:1
-#SBATCH --output=log.txt
+#SBATCH --job-name="multi-agent-rl"
+#SBATCH --output=srun.out
+#SBATCH --error=srun.err
+
+WORKDIR=${SLURM_SUBMIT_DIR}
+cd "${WORKDIR}" || exit 1
+echo "Running from this directory: $(pwd)"
+echo "Name of job: $SLURM_JOB_NAME"
+echo "ID of job: $SLURM_JOB_ID"
+echo "The jo was run on these nodes: $SLURM_JOB_NODELIST"
 
 module purge
 module load Python/3.11.5-GCCcore-13.2.0
@@ -16,3 +26,5 @@ poetry install
 poetry run wandb login
 
 poetry run python -O demo
+
+uname -a
