4baf764b208540586b5c12708874bc1ac2ccb432
Eirik Reiestad <eirik@reiestad.no>
2024-10-08 10:06:36 +0200

    feat: adding moving towards and away rewards

--- environments/gymnasium/envs/tag/tag_rewards.py
+++ environments/gymnasium/envs/tag/tag_rewards.py
@@ -1,11 +1,14 @@
 import math
 
+import numpy as np
+
 from environments.gymnasium.envs.tag import rewards
 from environments.gymnasium.utils import Position
 
 
 class TagRewards:
     max_distance = 1
+    last_distance = np.inf
 
     tagged_reward: tuple[float, float] = rewards.TAGGED_REWARD
     not_tagged_reward: tuple[float, float] = rewards.NOT_TAGGED_REWARD
@@ -16,6 +19,8 @@ class TagRewards:
     collision_reward: float = rewards.COLLISION_REWARD
     wrong_grab_release_reward: float = rewards.WRONG_GRAB_RELEASE_REWARD
     distance_factor: float = rewards.DISTANCE_REWARD_FACTOR
+    move_towards_reward: tuple[float, float] = rewards.MOVE_TOWARDS_REWARD
+    move_away_reward: tuple[float, float] = rewards.MOVE_AWAY_REWARD
 
     def reset(self):
         self.last_distance = 0
@@ -32,16 +37,28 @@ class TagRewards:
         normalized_distance = distance / self.max_distance
         exp_distance = 1 - math.exp(-normalized_distance)
 
+        seeker_distance_reward = (1 - exp_distance) * self.distance_factor
+        hider_distance_reward = exp_distance * self.distance_factor
+
+        if distance < self.last_distance:
+            self.last_distance = distance
+            seeker_distance_reward += self.move_towards_reward[0]
+            hider_distance_reward += self.move_towards_reward[1]
+        elif distance > self.last_distance:
+            self.last_distance = distance
+            seeker_distance_reward += self.move_away_reward[0]
+            hider_distance_reward += self.move_away_reward[1]
+
         if distance <= radius or terminated:
             tagged_reward = (
-                self.tagged_reward[0] + (1 - exp_distance) * self.distance_factor,
-                self.tagged_reward[1] + exp_distance * self.distance_factor,
+                self.tagged_reward[0] + seeker_distance_reward,
+                self.tagged_reward[1] + hider_distance_reward,
             )
             return tagged_reward, True
 
         not_tagged_reward = (
-            self.not_tagged_reward[0] + (1 - exp_distance) * self.distance_factor,
-            self.not_tagged_reward[1] + exp_distance * self.distance_factor,
+            self.tagged_reward[0] + seeker_distance_reward,
+            self.tagged_reward[1] + hider_distance_reward,
         )
         return not_tagged_reward, False
 
@@ -57,4 +74,6 @@ class TagRewards:
             "collision": self.collision_reward,
             "wrong_grab": self.wrong_grab_release_reward,
             "distance_factor": self.distance_factor,
+            "move_towards": self.move_towards_reward,
+            "move_away": self.move_away_reward,
         }
