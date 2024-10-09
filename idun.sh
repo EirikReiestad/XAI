e4b1725ee1fd2455c07d8a7af640dea663242830
Eirik Reiestad <eirik@reiestad.no>
2024-10-09 11:21:20 +0200

    model

--- idun.sh
+++ idun.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 #SBATCH --account="ie-idi"
 #SBATCH --partition=GPUQ
-#SBATCH --time=0-1:00:00
+#SBATCH --time=0-2:00:00
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
 #SBATCH --gres=gpu:1
