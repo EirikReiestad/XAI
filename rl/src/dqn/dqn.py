d0ff72f40c6887fa8b57593b20493eedc6b5b943
Eirik Reiestad <eirik@reiestad.no>
2024-10-09 12:46:34 +0200

    model: replay mem

--- rl/src/dqn/dqn.py
+++ rl/src/dqn/dqn.py
@@ -42,7 +42,7 @@ class DQN(SingleAgentBase):
         dueling: bool = False,
         double: bool = False,
         memory_size: int = 10000,
-        lr: float = 1e-3,
+        lr: float = 1e-4,
         gamma: float = 0.99,
         epsilon_start: float = 0.9,
         epsilon_end: float = 0.05,
@@ -96,7 +96,7 @@ class DQN(SingleAgentBase):
         optimizer = OptimizerManager(self.policy_net, self.hp.lr)
         self.optimizer = optimizer.initialize()
 
-        self.memory = MemoryManager(memory_size).initialize()
+        self.memory = MemoryManager(memory_size).initialize(memory_type="replay")
 
         self.double = double
         self.steps_done = 0
