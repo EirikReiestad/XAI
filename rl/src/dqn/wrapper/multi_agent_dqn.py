1a040ba3f3b0a27c68f1295a5df1d92796951cd3
Eirik Reiestad <eirik@reiestad.no>
2024-10-12 23:35:48 +0200

    debug

--- rl/src/dqn/wrapper/multi_agent_dqn.py
+++ rl/src/dqn/wrapper/multi_agent_dqn.py
@@ -77,7 +77,7 @@ class MultiAgentDQN(MultiAgentBase):
             logging.error("Wandb must be active to run a sweep.")
             return
         sweep_id = self.wandb_manager.sweep()
-        wandb.agent(sweep_id, lambda: self._run_agent_sweep(total_timesteps), count=100)
+        wandb.agent(sweep_id, lambda: self._run_agent_sweep(total_timesteps), count=1)
 
     def _run_agent_sweep(self, total_timesteps: int):
         self.wandb_manager.reinit()
