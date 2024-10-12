49d685e0d01d67940ba8cae7021f98593f0f94e2
Eirik Reiestad <eirik@reiestad.no>
2024-10-12 22:39:31 +0200

    debug!

--- rl/src/dqn/dqn.py
+++ rl/src/dqn/dqn.py
@@ -119,10 +119,10 @@ class DQN(SingleAgentBase):
         self._init_gif(gif, gif_path, gif_name)
 
     def reset(self) -> None:
-        return
         self.steps_done = 0
         self.episodes = 0
         self.eps_threshold = 0
+        return
         self.policy = PolicyManager().get_policy(
             self.dqn_policy,
             observation_space=self.env.observation_space,
