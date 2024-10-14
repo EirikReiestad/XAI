5691c15b1ecb5697ebe18dd87c6174e10f9369eb
Eirik Reiestad <eirik@reiestad.no>
2024-10-14 20:36:21 +0200

    model: save every n episodes from 50 to 100

--- demo/src/demos/multi_agent/tag.py
+++ demo/src/demos/multi_agent/tag.py
@@ -48,9 +48,15 @@ class TagDemo:
             wandb_active=False,
             wandb_config=wandb_config,
             model_name=model_name,
+<<<<<<< HEAD
             save_every_n_episodes=50,
             save_model=False,
             load_model=True,
+=======
+            save_every_n_episodes=100,
+            save_model=True,
+            load_model=False,
+>>>>>>> a4f1204 (model: save every n episodes from 50 to 100)
             gif=True,
             run_path="eirikreiestad-ntnu/tag-v0-eirre",
             model_artifact="model_1550",
