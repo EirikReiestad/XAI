b4b934393cadc142f743f253cb3aa1054c88f52f
Eirik Reiestad <eirik@reiestad.no>
2024-10-15 08:10:40 +0200

    model: smaller map

--- environments/gymnasium/envs/tag/tag.py
+++ environments/gymnasium/envs/tag/tag.py
@@ -51,7 +51,7 @@ class TagEnv(gym.Env):
         self.bootcamp = Bootcamp()
         self.tag_radius = 1
         self.tag_head_start = 0
-        self.max_steps = 400
+        self.max_steps = 200
         self.freeze_hider = False
         self.terminate_out_of_bounds = False
 
