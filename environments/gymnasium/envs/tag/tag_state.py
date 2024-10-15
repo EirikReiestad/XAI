0c4c2b49dbf8e4b79d36630f198f8c0a30d5e05e
Eirik Reiestad <eirik@reiestad.no>
2024-10-15 15:28:42 +0200

    model: slow hider and not random

--- environments/gymnasium/envs/tag/tag_state.py
+++ environments/gymnasium/envs/tag/tag_state.py
@@ -27,8 +27,8 @@ class TagState:
         self.screen_width = screen_width
         self.screen_height = screen_height
         self.state_type = state_type
-        self.random_seeker_position = True
-        self.random_hider_position = True
+        self.random_seeker_position = False
+        self.random_hider_position = False
         self.random_box_position = True
         self._init_states(filename)
         self._init_dimensions()
