047c4462d0025b764601e9c6dd8c131459c72c64
Eirik Reiestad <eirik@reiestad.no>
2024-10-16 15:16:38 +0200

    model

--- environments/gymnasium/envs/tag/utils/bootcamp.py
+++ environments/gymnasium/envs/tag/utils/bootcamp.py
@@ -38,7 +38,7 @@ class Bootcamp:
         self._bootcamp_num = 0
         self._num_bootcamps = 1
 
-        self.initial_slow_factor = 20
+        self.initial_slow_factor = 1
         self.slow_factors: list[int] = self._get_slow_factors(
             self.initial_slow_factor, self._num_bootcamps
         )
