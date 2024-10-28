cbe07996e5d9ad025f525ae15e6d4c526496f9d4
Eirik Reiestad <eirik@reiestad.no>
2024-10-28 21:51:09 +0100

    feat: normalized shap values

--- methods/src/shap/utils/normalize_states.py
+++ methods/src/shap/utils/normalize_states.py
@@ -1,10 +1,7 @@
 import numpy as np
 
 
-def normalize_states(states: np.ndarray) -> np.ndarray:
-    a = 0
-    b = 255
-
+def normalize_states(states: np.ndarray, a: float = 0, b: float = 1) -> np.ndarray:
     global_min = np.min(states)
     global_max = np.max(states)
 
