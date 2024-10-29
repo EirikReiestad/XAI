feat: hopefully fixed cav. The model doesn't seem to have any concepts yet, but I believe it is because of normalization (old model)

environments/gymnasium/envs/tag/tag_concepts.py
@@ -16,6 +16,10 @@ class TagConcepts:
             "A block that can be pushed around by the agent. It should be blocking thee path between the agents.",
         ),
         Concept(
+            "box-not-exist",
+            "The box does not exist in the environment.",
+        ),
+        Concept(
             "random", "An arbitrary concept that does not have a specific meaning."
         ),
     ]
@@ -38,10 +42,29 @@ class TagConcepts:
             raise ValueError(f"Concept {concept} not found in environment.")
         if concept == "box-block":
             return self._get_box_block_concept(samples)
+        elif concept == "box-not-exist":
+            return self._get_box_not_exist_concept(samples)
         elif concept == "random":
             return self._get_random_concept(samples)
         else:
             raise NotImplementedError(f"Concept {concept} is not implemented yet.")
+
+    def _get_box_not_exist_concept(
+        self, samples: int
+    ) -> tuple[list[np.ndarray], list[str]]:
+        self.state.random_seeker_position = True
+        self.state.random_hider_position = True
+        self.state.random_box_position = False
+        states = []
+        labels = []
+        for _ in range(samples):
+            self.state.reset()
+            self.state.remove_box()
+            states.append(self.state.normalized_full_state)
+            labels.append(
+                np.random.randint(self.num_actions)
+            )  # TODO: This should be changed to the correct action
+        return states, labels
 
     def _get_box_block_concept(
         self, samples: int
@@ -53,8 +76,10 @@ class TagConcepts:
         labels = []
         for _ in range(samples):
             self.state.reset()
-            states.append(self.state.full)
-            labels.append(np.random.randint(self.num_actions))
+            states.append(self.state.normalized_full_state)
+            labels.append(
+                np.random.randint(self.num_actions)
+            )  # TODO: This should be changed to the correct action
         return states, labels
 
     def _get_random_concept(self, samples: int) -> tuple[list[np.ndarray], list[str]]:
@@ -65,6 +90,6 @@ class TagConcepts:
         labels = []
         for _ in range(samples):
             self.state.reset()
-            states.append(self.state.full)
+            states.append(self.state.normalized_full_state)
             labels.append(np.random.randint(self.num_actions))
         return states, labels

environments/gymnasium/envs/tag/tag_state.py
@@ -33,6 +33,17 @@ class TagState:
         self._init_states(filename)
         self._init_dimensions()
 
+    def remove_box(self):
+        new_full_state, _ = FullStateDataModifier.remove_objects(
+            self.state.full, TileType.BOX
+        )
+        self.update(
+            new_full_state,
+            self.get_agent_position(AgentType.SEEKER),
+            self.get_agent_position(AgentType.HIDER),
+            Objects([], []),
+        )
+
     @property
     def init_full_state(self):
         init_state = self._init_full_state
@@ -60,12 +71,12 @@ class TagState:
     @property
     def active_state(self, normalized: bool = True) -> np.ndarray:
         if normalized:
-            return self.state.normalized_state
+            return self.state.normalized_full_state
         return self.state.active_state
 
     @property
-    def normalized_state(self) -> np.ndarray:
-        return self.state.normalized_state
+    def normalized_full_state(self) -> np.ndarray:
+        return self.state.normalized_full_state
 
     @property
     def full(self) -> np.ndarray:

methods/src/cav/analysis.py
@@ -17,11 +17,41 @@ class Analysis:
         self._models = models
         self._positive_sample_path = positive_sample_path
         self._negative_sample_path = negative_sample_path
-
         self._cav_scores = {}
         self._model_steps = {}
 
-    def run(self):
+        self._total_cav_scores = {}
+        self._average_cav_scores = {}
+
+    def run(self, averages: int = 10):
+        for _ in range(averages):
+            self._run_cav()
+            self._add_total_cav_scores()
+
+        self._calculate_average_cav_scores(averages)
+
+    def _add_total_cav_scores(self):
+        for model, layers in self._cav_scores.items():
+            if model not in self._total_cav_scores:
+                self._total_cav_scores[model] = {}
+            for (
+                layer,
+                score,
+            ) in layers.items():
+                if layer not in self._total_cav_scores:
+                    self._total_cav_scores[model][layer] = 0
+                self._total_cav_scores[model][layer] += score
+
+    def _calculate_average_cav_scores(self, n: int):
+        for model, layers in self._cav_scores.items():
+            self._average_cav_scores[model] = {}
+            for (
+                layer,
+                score,
+            ) in layers.items():
+                self._average_cav_scores[model][layer] = score / n
+
+    def _run_cav(self):
         while True:
             cav = CAV(
                 self._models.policy_net,
@@ -40,13 +70,12 @@ class Analysis:
 
     def plot(self, save_path: str = "cav_plot.png"):
         matrix = np.array(
-            [list(scores.values()) for scores in self._cav_scores.values()]
+            [list(scores.values()) for scores in self._average_cav_scores.values()]
         )
         steps = [f"{step:.1e}" for step in self._model_steps.values()]
         if len(matrix) == 0:
             logging.error("No CAV scores found.")
             return
-        matrix_flat = matrix.flatten()
 
         fig = plt.figure()
         ax1 = fig.add_subplot(projection="3d")
@@ -56,12 +85,7 @@ class Analysis:
         _xx, _yy = np.meshgrid(_x, _y)
         x, y = _xx.ravel(), _yy.ravel()
 
-        top = matrix_flat
-        bottom = np.zeros_like(top)
-        width = depth = 1
-
-        ax1.bar3d(x, y, bottom, width, depth, top, shade=True)
-
+        ax1.plot_surface(_xx, _yy, matrix, cmap="coolwarm", edgecolor="k")
         ax1.set_zlim(0, 1)
         ax1.set_title("CAV Scores")
         ax1.set_xlabel("Layer")