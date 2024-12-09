0ad9ee04fdca5ff5b2f3786cfa78a8057d1a6451
Eirik Reiestad <eirik@reiestad.no>
2024-12-09 12:43:09 +0100

    model

--- rl/src/dqn/dqn.py
+++ rl/src/dqn/dqn.py
@@ -46,7 +46,7 @@ class DQN(SingleAgentBase):
         gamma: float = 0.99,
         epsilon_start: float = 1.0,
         epsilon_end: float = 0.05,
-        epsilon_decay: float = 0.000003,
+        epsilon_decay: float = 0.000010,
         slow_decay_point: float = 1.0,
         slow_decay_factor: float = 1.0,
         batch_size: int = 32,
